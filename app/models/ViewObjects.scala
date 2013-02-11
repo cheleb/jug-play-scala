2fce47a04261e5d4ec1cae1e64420023200d3efd
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2013-02-11 13:19:53 +0100

    Add default img when email is missing.

--- app/models/ViewObjects.scala
+++ app/models/ViewObjects.scala
@@ -1,33 +1,36 @@
 package models
 
-
 import java.io._
 import java.security._
+import controllers.routes
 
 case class EventViewObject(event: Event, talks: List[Talk], speakers: List[Speaker]) {
-  
-  def hex(array: Array[Byte]) = {
+
+  private def hex(array: Array[Byte]) = {
     val sb = new StringBuffer()
-    for (i <- 0 to array.length -1)
+    for (i <- 0 to array.length - 1)
       sb.append(Integer.toHexString((array(i)
         & 0xFF) | 0x100).substring(1, 3));
 
     sb.toString();
   }
-  
-  def md5Hex(message: String) = {
+
+  private def md5Hex(message: String) = {
     try {
       val md =
         MessageDigest.getInstance("MD5");
       hex(md.digest(message.getBytes("UTF-8")));
     } catch {
       case e: NoSuchAlgorithmException => "nop"
-      case e: UnsupportedEncodingException =>"nop"
+      case e: UnsupportedEncodingException => "nop"
     }
   }
 
   def gravatar(speaker: Speaker) = {
-    "http://www.gravatar.com/avatar/" + md5Hex(speaker.email.get)
+    speaker.email match {
+      case Some(e) => "http://www.gravatar.com/avatar/" + md5Hex(e)
+      case _ => routes.Assets.at("images/none.jpg")
+    }
   }
 
 }
